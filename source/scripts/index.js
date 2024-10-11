f833a91c4b95a65624ba636add68226ccc54c8a4
Artur Pletnev <art-pletnev@mail.ru>
2024-10-11 20:13:30 +0300

    правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const toggleButtonElement = document.querySelector('.header__button-toggle');
+const menuElement = document.querySelector('.header__nav');
+
+toggleButtonElement.addEventListener('click', () => {
+  toggleButtonElement.classList.toggle('header__button-toggle--open');
+  menuElement.classList.toggle('header__nav--opened');
+});
